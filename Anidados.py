15035b1d82492622ac6ce0b2bbbcdf1cac2d1d38
JuanMacias07x <80846275+JuanMacias07x@users.noreply.github.com>
2021-03-24 16:21:59 -0500

    Add files via upload

---
+++ Anidados.py
@@ -0,0 +1,16 @@
+#Primer anidado
+
+for h in range (5):
+  for l in range(h):
+    print ("h vale", h, "y l vale", l)
+
+#Segundo anidado
+
+for z in range (8):
+  for r in range(z):
+    print ("z vale", z, "y l vale", r)
+
+
+
+
+
